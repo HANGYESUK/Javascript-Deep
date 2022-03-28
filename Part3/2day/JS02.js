9fcf8f4b68fdabc2d68e7a34024848781299f483
HANGYESUK <akakakak66@naver.com>
2022-03-28 20:13:47 +0900

    Part3(2day) - import/export, 브라우저동작원리(스택,힙,큐), Promise패턴

--- Part3/2day/JS02.js
+++ Part3/2day/JS02.js
@@ -1,45 +1,37 @@
-// Reference data type - Array, Object는 변수에 값을 저장 안하고 reference를 저장함
-// 그냥 문자와 숫자는 Primitive data type - 변수에 값이 그대로 저장
-let num = 1234;
-let string = '1234';
+//inport export를 이용한 모듈식 개발
+// html 스크립트 태그에 type="module" 쓰면 사용가능
+<script type="module"></script>
 
-// Array, Object는 reference를 저장 - 값이 저장된 메로리 주소 저장
-let arr = [1,2,3,4] 
-let obj = { name : 'kim' }
+import a from './library.js';
 
 
+//브라우저 동작원리
+//힙 스택 큐
+//싱글스레드 언어 - 스택에서 코드 하나씩 실행
+//오래걸리는 함수나 코드를 대기실로 넘김
+//대기실 => 큐 => 스택(비었을때) - 대표적으로 setTime, 이벤트리스너
+console.log(1) //1빠
+setTimeout(()=>{console.log(2)}, 1000) //3빠(바로 실행안되니까)
+console.log(3) //2빠
 
-// 파라미터와 레퍼런스데이터
-let name1 = { name: 'park' };
 
-function change(obj) {
-    obj = { name: 'kim' }
+//콜백함수 - 코드가 복잡해짐
+// Promise패턴으로 해결 
+function Func1(prameter) {
+    console.log(1)
+    prameter()
 }
 
-// 파라미터는 change(var obj = name1)이랑 같음
-change(name1)
-
-
-
-
-//constructor - 생성자
-// 오브젝트 형태가 리턴됨
-function Func1() {
-    this.name = 'kim',
-    this.age = 26
+function Func2() {
+    console.log(2)
 }
 
-let std1 = new Func1()
+Func1(Func2);
 
 
+//Promise 패턴 - .then().catch()
+Func1().then(function() {
 
-let std2 = new Func1();
+}).then(function () {
 
-std2.sayHi = ()=>{
-    console.log("안녕하세요" + this.name)
-}
-
-std2.sayHi()
-
-
-
+}).catch()
