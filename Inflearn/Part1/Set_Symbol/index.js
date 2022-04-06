811e260ea00b07431c4d3fa2fcf26633a4b665d9
HANGYESUK <akakakak66@naver.com>
2022-04-06 17:17:40 +0900

    feat: 인프런 강의 - set, map

--- Inflearn/Part1/Set_Symbol/index.js
+++ Inflearn/Part1/Set_Symbol/index.js
@@ -1,1 +1,12 @@
 // ES6문법
+
+// 새롭게 도입한 데이터 자료구조 - map, set
+// map은 object와 비슷함(key, value)
+// set은 중복을 허용하지 않음(array와 비슷함)
+
+// set사용법
+// 생성 - new, 추가 - add, 삭제 - delete
+let arr = new Set();
+arr.add(1,2,3);
+arr.add(3,4,5);
+console.log(arr);
